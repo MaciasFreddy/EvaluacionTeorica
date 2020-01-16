f27970b7d506b4da7a7e6fc73ea75655cc0593c4
MaciasFreddy <58390329+MaciasFreddy@users.noreply.github.com>
2020-01-16 14:09:59 -0500

    Add files via upload

---
+++ PruebaSingleton/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PruebaSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine(Singleton.InstanciaClase.Tipo);
+
+            // aqui por medio de la instancia se va a enviar a imprimir los tipos de carros a traves del objeto
+            Singleton.InstanciaClase.Tipo = "Carros Electricos";
+            Console.WriteLine(Singleton.InstanciaClase.Tipo);
+
+            Singleton.InstanciaClase.Tipo = "Carros Gasolina";
+            Console.WriteLine(Singleton.InstanciaClase.Tipo);
+
+            Singleton.InstanciaClase.Tipo = "Carros Diesel";
+            Console.WriteLine(Singleton.InstanciaClase.Tipo);
+        }
+    }
+}
---
+++ PruebaSingleton/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PruebaSingleton
+{
+    class Singleton
+    {
+        private static Singleton problema = null;
+
+        public static Singleton InstanciaClase   // este es un metodo  que retorna la instancia en caso de que no halla.
+        {
+            get
+            {
+                if (problema == null)
+                {
+                    problema = new Singleton();
+                }
+                return problema;
+            }
+        }
+
+        public string Tipo = ""; // este es una propiedad 
+        public Singleton()  // este es el constructor de la clase
+        {
+            Tipo = "Tipos De Carros";  // aqui enviamos el tipo de carro que va a imprimir
+        }
+    }
+}
